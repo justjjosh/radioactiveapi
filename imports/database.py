e4ecb4626a771691515804e881215a558cc85e1b
Raji Olatubosun Joshua <rajiolatubosun@gmail.com>
2026-07-15 18:53:08 +0100

    wrote the sqlalchemy and database session connection layer

---
+++ imports/database.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+Database_URL = os.getenv("DATABASE_URL")
+if not Database_URL:
+    raise ValueError("DATABASE_URL environment variable is not set.")
+
+engine = create_engine(Database_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+#parent class that all other database models will inherit from.
+Base = declarative_base()
+
+#dependency that will be injected into FASTAPI routes to get a database session.
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
